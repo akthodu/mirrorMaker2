490a3d8a87f92f77a0374f97f4271f0410fd5539
apabhat <66872446+apabhat@users.noreply.github.com>
2020-08-08 14:03:52 +0530

    Update Source_mm2_offset.java

--- kafka2/src/Source_mm2_offset.java
+++ kafka2/src/Source_mm2_offset.java
@@ -11,7 +11,7 @@ public class Source_mm2_offset{
             
             String groupName = "RG";
             Properties props = new Properties();
-            props.put("bootstrap.servers", "pc021.fyre.ibm.com:9092");
+            props.put("bootstrap.servers", "host21:9092");
             props.put("group.id", groupName);
             props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
             props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
